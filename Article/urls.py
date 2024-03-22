883fab2a3b4c90630884830c76ae3b9ca68e1f0d
unknown-zyz <805290114@qq.com>
2024-03-22 11:29:46 +0800

    add LLM

--- Article/urls.py
+++ Article/urls.py
@@ -2,10 +2,11 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', ArticleListOrderedByDate.as_view()), # /articles/?page=2
+    path('', ArticleListOrderedByDate.as_view()),
     path('read/', ArticleListOrderedByRead.as_view()),
     path('detail/<int:article_id>/', ArticleDetail.as_view()),
     path('search/', SearchArticle.as_view()),
+    path('summary/<int:article_id>/', SummaryArticle.as_view()),
     # path('create/', create_article),
 
 ]
